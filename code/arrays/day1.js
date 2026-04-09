a3b9987f936daa7e5b6b9cba6c5f256d7a9a77c0
Mukul-bairwa-codeZ9 <mukul.bairwa.dev98@gmail.com>
2026-04-09 23:18:59 +0530

    feat(code): add day3.js

--- code/arrays/day1.js
+++ code/arrays/day1.js
@@ -1,17 +1,5 @@
 // find occurences of each element in arrary
-
-const arr = [1, 6, 8, 2, 0, 2, 5, 2, 4, 2, 5, 1, 0, 11, 89, 16, 1, 89];
-
-function findOccurences(data) {
-  let occurences = {};
-  data.forEach((el, idx) => {
-    if (occurences[el] !== undefined) {
-      occurences[el]++;
-    } else occurences[el] = 1;
-  });
-  return occurences;
-}
-console.log(findOccurences(arr));
+c
 
 
 // find most frequent element in arrary
