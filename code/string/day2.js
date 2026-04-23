b583ad535fe80a0a6e3c9d876501acaa9b013dc4
Mukul-bairwa-codeZ9 <mukul.bairwa.dev98@gmail.com>
2026-04-23 22:42:19 +0530

    feat(code): add check palindrome string

---
+++ code/string/day2.js
@@ -0,0 +1,12 @@
+// check palidrome
+
+
+let str = "mukum"
+function isPalindrome(s){
+    let newStr =''
+    for(let i=s.length-1;i>=0;i--){
+        newStr+= s[i]
+    }
+    return newStr === s  ? true : false
+}
+console.log(isPalindrome(str))
